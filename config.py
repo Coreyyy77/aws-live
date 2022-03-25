5208fa3a4ba9291d3d2ea276410dc5c7afbb35b3
Coreyyy77 <99870545+Coreyyy77@users.noreply.github.com>
2022-03-25 23:27:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.caei5yssob5s.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "bait3273"
 customdb = "employee"
 custombucket = "ongchoonteck-employee"
 customregion = "us-east-1"
